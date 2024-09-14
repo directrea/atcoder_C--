1d7b73142fe33d16b168be2035abd545e531262d
Shun <progreawa.main@gmail.com>
2024-09-14 10:41:17 +0900

    a

--- test.cpp
+++ test.cpp
@@ -9,19 +9,7 @@ template <class T> void print(T out, string end = "\n") { cout << out << end; }
 template <class T> void print(vec<T> v) { rep(i, v.size()) cout << v[i] << " \n"[i == (int)v.size() - 1]; }
 
 int main() {
-  ll x, a, d, n;
-  cin >> x >> a >> d >> n;
-  if (d < 0) x = -x, a = -a, d = -d;
-
-  ll top = 0, tail = n;
-  while (top + 1 < tail) {
-    ll mid = (top + tail) / 2;
-    if (a + d * mid <= x)
-      top = mid;
-    else
-      tail = mid;
-  }
-  print(abs(x - (a + d * top)));
+  
 
   return 0;
 }
